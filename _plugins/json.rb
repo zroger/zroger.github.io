895c4c6fee11c522f5b0b6ab9061c27697b170fb
Roger López <lopez.roger@gmail.com>
2013-08-16 13:30:39 -0400

    add paginator to blogs

--- _plugins/json.rb
+++ _plugins/json.rb
@@ -1,16 +1,9 @@
-# Convert liquid objects to json.  Very useful for display in a javascript console.
-#  <script type="text/javascript">
-#    console.log("site: %O", {{ site | json }});
-#  </script>
-
 require 'json'
 
-module Jekyll
-  module JsonFilter
-    def json(obj)
-      obj.to_json.gsub /\/script/, '\/script'
-    end
+module JsonFilter
+  def json(input)
+    input.to_json
   end
-end
 
-Liquid::Template.register_filter(Jekyll::JsonFilter)
+  Liquid::Template.register_filter self
+end
