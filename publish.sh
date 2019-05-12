2b7695063314659f6a25e4e2a4bca622854e5063
Roger López <zroger@users.noreply.github.com>
2019-05-12 17:23:23 -0400

    Add publishing script

---
+++ publish.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing via publish.sh"
